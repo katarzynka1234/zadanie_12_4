226cd2970149f7736acd0f5b72f28c5df7b97534
Katarzyna Trześniewska <katarzynka.f@gmail.com>
2018-04-25 15:11:36 +0200

    Code format

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 var url = 'http://api.icndb.com/jokes/random';
 
 var button = document.getElementById('get-joke');
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
     getJoke();
 });
 
@@ -11,7 +11,7 @@ var paragraph = document.getElementById('joke');
 function getJoke() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url);
-    xhr.addEventListener('load', function(){
+    xhr.addEventListener('load', function () {
         var response = JSON.parse(xhr.response);
         paragraph.innerHTML = response.value.joke;
     });
